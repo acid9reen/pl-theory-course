1c4292fe7c2eda5cd9163683e088e0dba1c9a099
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2023-10-30 22:08:01 +0300

    Add cbn

--- pcf/pcf1.hs
+++ pcf/pcf1.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Control.Monad (when)
 
 data Term =
     Const Int
@@ -103,38 +104,69 @@ fv (Let x t u) = nub $ fv t ++ delete x (fv u)
 -- и вызовом по значению, согласно правилам в разделе 3.2
 -- книги Довека, Леви.
 
+cbn :: Env -> Term -> Value
+cbn e (Const n) = ValInt n
+cbn e (Var x) = case lookup x e of
+    Just (Thunk (Fix y t) e') -> cbn (extendEnv e' y (Thunk (Fix y t) e')) t
+    Just (Thunk t e') -> cbn e' t
+    Just v -> v -- обычное (не оснащённое) значение
+    Nothing -> error ("Variable " ++ x ++ " is not bound.")
+cbn e (Abs x t) = Closure x t e
+cbn e (App t u) = case cbn e t of
+    Closure x t' e' -> cbn (extendEnv e' x (Thunk u e)) t'
+    _ -> error "Applying a non-function value."
+cbn e (Plus t u) = case (cbn e t, cbn e u) of
+    (ValInt n, ValInt m) -> ValInt (n + m)
+    _ -> error "Invalid addition."
+cbn e (Minus t u) = case (cbn e t, cbn e u) of
+    (ValInt n, ValInt m) -> ValInt (n - m)
+    _ -> error "Invalid subtraction."
+cbn e (Times t u) = case (cbn e t, cbn e u) of
+    (ValInt n, ValInt m) -> ValInt (n * m)
+    _ -> error "Invalid multiplication."
+cbn e (Ifz c t u) = case cbn e c of
+    ValInt 0 -> cbn e t
+    ValInt _ -> cbn e u
+    _ -> error "Invalid condition."
+cbn e (Fix f t) = cbn (extendEnv e f (Thunk (Fix f t) e)) t
+cbn e (Let x t u) = cbn (extendEnv e x (Thunk t e)) u
+
 cbv :: Env -> Term -> Value
 cbv e (Const n) = ValInt n
 cbv e (Var x) = case lookup x e of
-                  Just (Thunk (Fix y t) e') -> cbv (extendEnv e' y (Thunk (Fix y t) e')) t
-                  Just (Thunk t e') -> cbv e' t
-                  Just v -> v -- обычное (не оснащённое) значение
-                  Nothing -> error ("Variable " ++ x ++ " is not bound.")
+    Just (Thunk (Fix y t) e') -> cbv (extendEnv e' y (Thunk (Fix y t) e')) t
+    Just v -> v -- обычное (не оснащённое) значение
+    Nothing -> error ("Variable " ++ x ++ " is not bound.")
 cbv e (Abs x t) = Closure x t e
 cbv e (App t u) = case cbv e t of
-                    Closure x t' e' -> cbv (extendEnv e' x (Thunk u e)) t'
-                    _ -> error "Applying a non-function value."
+    Closure x t' e' -> cbv (extendEnv e' x (cbv e u)) t'
+    _ -> error "Applying a non-function value."
 cbv e (Plus t u) = case (cbv e t, cbv e u) of
-                     (ValInt n, ValInt m) -> ValInt (n + m)
-                     _ -> error "Invalid addition."
+    (ValInt n, ValInt m) -> ValInt (n + m)
+    _ -> error "Invalid addition."
 cbv e (Minus t u) = case (cbv e t, cbv e u) of
-                      (ValInt n, ValInt m) -> ValInt (n - m)
-                      _ -> error "Invalid subtraction."
+    (ValInt n, ValInt m) -> ValInt (n - m)
+    _ -> error "Invalid subtraction."
 cbv e (Times t u) = case (cbv e t, cbv e u) of
-                      (ValInt n, ValInt m) -> ValInt (n * m)
-                      _ -> error "Invalid multiplication."
+    (ValInt n, ValInt m) -> ValInt (n * m)
+    _ -> error "Invalid multiplication."
 cbv e (Ifz c t u) = case cbv e c of
-                      ValInt 0 -> cbv e t
-                      ValInt _ -> cbv e u
-                      _ -> error "Invalid condition."
+    ValInt 0 -> cbv e t
+    ValInt _ -> cbv e u
+    _ -> error "Invalid condition."
 cbv e (Fix f t) = cbv (extendEnv e f (Thunk (Fix f t) e)) t
-cbv e (Let x t u) = cbv (extendEnv e x (Thunk t e)) u
+cbv e (Let x t u) = cbv (extendEnv e x (cbv e t)) u
 
 emptyEnv :: Env
 emptyEnv = []
 
 runCbv :: Term -> Value
 runCbv = cbv emptyEnv
+
+
+runCbn :: Term -> Value
+runCbn = cbn emptyEnv
+
 -- Проверьте работу cbv на термах t1, ..., t5.
 -- Напишите функцию, вычисляющую факториал и проверьте ее работу
 -- с помощью cbv.
